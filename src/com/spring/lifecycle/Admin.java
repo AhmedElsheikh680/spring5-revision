696fa484d944e1d14737754227eed94d589d51d6
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2022-05-24 06:10:31 -0700

    @PostConstruct @PreDestroy

--- src/com/spring/lifecycle/Admin.java
+++ src/com/spring/lifecycle/Admin.java
@@ -1,16 +1,27 @@
 package com.spring.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope("prototype")
 public class Admin implements PersonInterface {
 
+	@PostConstruct
+	public void init() {
+		System.out.println("Connected TO DB");
+	}
 	@Override
 	public void name() {
 		System.out.println("Iam User");
 		
+	}
+	@PreDestroy
+	public void destroy() {
+		System.out.println("Not Connected");
 	}
 
 }
--- src/com/spring/lifecycle/Main.java
+++ src/com/spring/lifecycle/Main.java
@@ -7,10 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		PersonInterface admin = context.getBean("admin", PersonInterface.class);
-		System.out.println(admin);
-		PersonInterface admin2 = context.getBean("admin", PersonInterface.class);
-		System.out.println(admin2);
-		System.out.println(admin == admin2);
+		admin.name();
+
 		context.close();
 
 	}
